9162dc21661c50c76acbb9412945aa315f155134
luckychacha <you@example.com>
2025-05-16 11:57:35 +0800

    feat: add img resources.

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,35 @@
 use makepad_widgets::*; // Import Makepad Widgets package
 
+
 // Define live_design macro for declaring UI components and layout
 live_design! {
     // import Makepad theme and shaders, and widgets
     use link::theme::*;
     use link::shaders::*;
     use link::widgets::*;
-    App = {{App}} {
-        ui: <Window> { }
+    PLACEHOLDER_IMAGE = dep("crate://self/resources/placeholder.jpg");
+
+
+    ImageItem = <View> {
+        width: 256,
+        height: 256,
+
+        image = <Image> {
+            width: Fill,
+            height: Fill,
+            source: (PLACEHOLDER_IMAGE),
+            fit: Biggest,
         }
     }
+
+    App = {{App}} {
+        ui: <Window> {
+            body = <View> {
+                <ImageItem> {}
+            }
+        }
+    }
+}
 
 
 // Define App struct containing UI and counter
