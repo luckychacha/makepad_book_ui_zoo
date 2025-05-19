d895934e4156559e82bc7c710c2d04a07bcb3184
luckychacha <luckychachaa@gmail.com>
2025-05-20 07:43:12 +0800

    feat: image grid

--- src/app.rs
+++ src/app.rs
@@ -30,11 +30,19 @@ live_design! {
         }
     }
 
+    ImageGrid = {{ImageGrid}} {
+        <PortalList> {
+            flow: Down,
+            ImageRow = <ImageRow> {}
+        }
+    }
+
     App = {{App}} {
         ui: <Window> {
             body = <View> {
                 // <ImageItem> {}
-                <ImageRow> {}
+                // <ImageRow> {}
+                <ImageGrid> {}
             }
         }
     }
@@ -58,6 +66,8 @@ impl Widget for ImageRow {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
                 list.set_item_range(cx, 0, 4);
                 while let Some(item_idx) = list.next_visible_item(cx) {
+                    // log the item index
+                    println!("item_idx: {}", item_idx);
                     let item = list.item(cx, item_idx, live_id!(ImageItem));
                     item.draw_all(cx, &mut Scope::empty());
                 }
@@ -70,6 +80,41 @@ impl Widget for ImageRow {
         self.view.handle_event(cx, event, scope)
     }
 }
+
+#[derive(Live, LiveHook, Widget)]
+pub struct ImageGrid {
+    #[deref]
+    view: View,
+}
+
+impl Widget for ImageGrid {
+    fn draw_walk(
+        &mut self, 
+        cx: &mut Cx2d, 
+        scope: &mut Scope, 
+        walk: Walk
+    ) -> DrawStep {
+        while let Some(item) =self.view.draw_walk(cx, scope, walk).step() {
+            if let Some(mut list) = item.as_portal_list().borrow_mut() {
+                list.set_item_range(cx, 0, 3);
+                while let Some(item_idx) = list.next_visible_item(cx) {
+                    // log the item index
+                    println!("item_idx: {}", item_idx);
+                    let item = list.item(cx, item_idx, live_id!(ImageRow));
+                    item.draw_all(cx, &mut Scope::empty());
+                }
+            }
+        }
+        DrawStep::done()
+    }
+
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        self.view.handle_event(cx, event, scope)
+    }
+}
+
+
+
 
 
 
