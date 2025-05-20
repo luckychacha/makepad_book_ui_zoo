6e371e2bc3cb7e458cae945c6d5493f0a21bd404
luckychacha <luckychachaa@gmail.com>
2025-05-21 07:17:21 +0800

    feat: use state

--- src/app.rs
+++ src/app.rs
@@ -96,8 +96,12 @@ impl Widget for ImageGrid {
     ) -> DrawStep {
         while let Some(item) =self.view.draw_walk(cx, scope, walk).step() {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
-                list.set_item_range(cx, 0, 3);
+                let state = scope.data.get_mut::<State>().unwrap();
+                list.set_item_range(cx, 0, state.num_rows());
                 while let Some(item_idx) = list.next_visible_item(cx) {
+                    if item_idx >= state.num_rows() {
+                        continue;
+                    }
                     // log the item index
                     println!("item_idx: {}", item_idx);
                     let item = list.item(cx, item_idx, live_id!(ImageRow));
@@ -113,16 +117,47 @@ impl Widget for ImageGrid {
     }
 }
 
+pub struct State {
+    num_images: usize,
+    max_images_per_row: usize,
+}
 
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            num_images: 12,
+            max_images_per_row: 5,
+        }
+    }
+}
 
+impl State {
+    fn num_images(&self) -> usize {
+        self.num_images
+    }
 
+    fn num_rows(&self) -> usize {
+        self.num_images().div_ceil(self.max_images_per_row)
+    }
 
+    fn first_image_idx_for_row(&self, row_idx: usize) -> usize {
+        row_idx * self.max_images_per_row
+    }
+
+    fn num_images_for_row(&self, row_idx: usize) -> usize {
+        let first_image_idx = self.first_image_idx_for_row(row_idx);
+        let remaining_images = self.num_images() - first_image_idx;
+        self.max_images_per_row.min(remaining_images)
+    }
+}
 
 // Define App struct containing UI and counter
 #[derive(Live, LiveHook)]
 pub struct App {
     #[live]
     ui: WidgetRef, // UI component reference
+    #[rust]
+    state: State,
 }
 
 // Implement LiveRegister trait for registering live design
@@ -136,8 +171,9 @@ impl LiveRegister for App {
 // Implement AppMain trait for handling events
 impl AppMain for App {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        let mut scope = Scope::with_data(&mut self.state);
         // Handle UI events
-        self.ui.handle_event(cx, event, &mut Scope::empty());
+        self.ui.handle_event(cx, event, &mut scope);
     }
 }
 
